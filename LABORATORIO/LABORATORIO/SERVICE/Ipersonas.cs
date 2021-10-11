0e58913fdac2cefa2eb7b1581365d8cfd12c01b2
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-10-11 14:49:06 -0600

    1CAMBIO

---
+++ LABORATORIO/LABORATORIO/SERVICE/Ipersonas.cs
@@ -0,0 +1,14 @@
+﻿using LABORATORIO.DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO.SERVICE
+{
+    interface Ipersonas
+    {
+        void guardar(persona persona);
+    }
+}
