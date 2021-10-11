d994bf4b8ec73a67a6b65102395c4d69c6857900
july22lop <88002008+july22lop@users.noreply.github.com>
2021-10-11 15:56:59 -0600

    Cambio4

--- LABORATORIO/LABORATORIO/NEGOCIO/Clspersona.cs
+++ LABORATORIO/LABORATORIO/NEGOCIO/Clspersona.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using LABORATORIO.DATA;
+using LABORATORIO.REPOSITORY;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace LABORATORIO.NEGOCIO
 {
-    class Clspersona
+    class Clspersona : ClsRpersona
     {
+        public void GUARDADON(persona pers)
+        {
+            if (pers.edadPersona < 18)
+            {
+                MessageBox.Show("LA PERSONA ES MENOR DE EDAD " +
+                    "\n NO SE PUEDE REGISTRAR");
+            }
+            else
+            {
+                guardar(pers);
+            }
+        }
     }
 }
