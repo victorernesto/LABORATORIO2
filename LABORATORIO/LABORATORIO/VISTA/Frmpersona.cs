410395b3341c2cece90513a33a287a6d0b6f1474
victorernesto <88003634+victorernesto@users.noreply.github.com>
2021-10-11 18:10:04 -0600

    6CAMBIO

---
+++ LABORATORIO/LABORATORIO/VISTA/Frmpersona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LABORATORIO.VISTA
+{
+    public partial class Frmpersona : Form
+    {
+        public Frmpersona()
+        {
+            InitializeComponent();
+        }
+    }
+}
